bd9edd902dbd112a9e17b41bca3e0c9d35bf062b
Mateus Sena <mateussantos192007@gmail.com>
2026-02-16 22:47:22 -0300

    Update index.jsx

--- cadastro-usuarios/src/pages/Home/index.jsx
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,14 +1,49 @@
 import './style.css'
+import Trash from '../../assets/trash.svg'
+import { use } from 'react'
 
 function Home() {
-
+  const users = [{
+    id: '23232jjjjjj',
+    name: 'Mateus',
+    age: 19,
+    email: 'mateus@gmail.com'
+  },
+  {
+    id: '232323dsdsdsdsa',
+    name: 'Senna',
+    age: 28,
+    email: 'senna@gmail.com'
+  }]
   return (
-    
-      <div>
-        <h1>Olá</h1>
-      </div>
-      
+
+    <div className='container'>
+      <form action="">
+        <h1>Cadastro de Usuários</h1>
+
+        <input placeholder='Nome' name='nome' type="text" />
+        <input placeholder='Idade' name='idade' type="number" />
+        <input placeholder='Email' name='email' type="email" />
+
+        <button type='button'>Cadastrar</button>
+
+      </form>
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash}/>
+          </button>
+        </div>
+      ))}
+    </div>
+
+
   )
 }
 
-export default App
+export default Home
